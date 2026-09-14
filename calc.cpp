54cde75ca8ec72461d00b6a1a127c054c8891e21
wu/echo <208830558@qq.com>
2026-09-14 15:03:33 +0800

    day1:第一个c++和python程序

---
+++ calc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<<"请输入第一个数字：";
+    cin >> num1;
+
+    cout<<"请输入第二个数字: ";
+    cin >>num2;
+
+    cout<<num1<<"+"<<num2<<"="<<num1+num2<<endl;
+    cout<<num1<<"-"<<num2<<"="<<num1-num2<<endl;
+
+    return 0;
+}
--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    cout << "Hello, Ubuntu C++!" << endl;
+int main(){
+
+    cout <<"Hello,C++"<<endl;
     return 0;
 }
